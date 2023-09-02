4366aa399fb6c96a205ace382763af70704c3a21
Ricardo Zubieta <rgzd7@hotmail.com>
2023-09-02 00:49:19 -0600

    se avanzo hasta el modulo 4, queda pendiente functionEsEntero en adelante

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,9 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   if ( x > y || x===y) return (x);
+   else ( x < y) 
+   return (y);
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +15,8 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if (edad >= 18) return ("Allowed");
+   else return ("Not allowed");
 }
 
 function conection(status) {
@@ -21,6 +26,9 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   if (status === 1) return ("Online");
+   else if (status === 2) return ("Away");
+   else return ("Offline");
 }
 
 function saludo(idioma) {
@@ -30,6 +38,19 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   switch (idioma) {
+      case "aleman":
+         return ("Guten Tag!");
+      break; 
+      case "mandarin":
+         return ("Ni Hao!");
+      break;
+      case "ingles":
+         return ("Hello!");
+       break;  
+      default:
+         return ("Hola!");   
+   }
 }
 
 function colors(color) {
@@ -41,18 +62,39 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   switch (color) {
+      case "blue":
+         return ("This is blue");
+      break; 
+      case "red":
+         return ("This is red");
+      break;
+      case "green":
+         return ("This is green");
+       break;
+      case "orange":
+         return ("This is orange");
+       break;     
+      default:
+         return ("Color not found");   
+   }
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   if (num === 5 || num === 10) return (true);
+   else return (false);
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   if (num > 20 && num < 50)
+   return (true);
+   else return (false);
 }
 
 function esEntero(num) {
@@ -62,6 +104,14 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   if (num !== Number.isInteger(num)) {
+      return (true);
+   }
+   else {
+      return (false);
+   }
+ 
+   
 }
 
 function fizzBuzz(num) {
